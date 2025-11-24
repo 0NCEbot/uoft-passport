01ddc041a35a64f3e722399245955ff63e6f3e53
Nathan <nathanallen668@gmail.com>
2025-11-24 00:13:56 -0500

    editnote input data code

--- src/main/java/use_case/editnote/EditNoteInputData.java
+++ src/main/java/use_case/editnote/EditNoteInputData.java
@@ -1,4 +1,19 @@
 package use_case.editnote;
 
 public class EditNoteInputData {
+    private final String noteId;
+    private final String newContent;
+
+    public EditNoteInputData(String noteId, String newContent) {
+        this.noteId = noteId;
+        this.newContent = newContent;
+    }
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public String getNewContent() {
+        return newContent;
+    }
 }
