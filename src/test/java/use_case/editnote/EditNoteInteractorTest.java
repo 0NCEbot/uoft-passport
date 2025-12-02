b14541f022291b9722c53ff691e224393bce029a
Nathan <nathanallen668@gmail.com>
2025-12-02 16:05:41 -0500

    pushing again since originial in wrong repository

--- src/test/java/use_case/editnote/EditNoteInteractorTest.java
+++ src/test/java/use_case/editnote/EditNoteInteractorTest.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//edits
 class EditNoteInteractorTest {
 
     private TestDataAccess dataAccess;
